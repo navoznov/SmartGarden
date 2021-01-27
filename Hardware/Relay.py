cde5cd02721e37bb60cd439743bd0526b28ee5ef
Navoznov Ivan <ivan.navoznov@gmail.com>
2021-01-28 00:28:41 +0300

    добавлены комментарии к полям класса Relay

--- Hardware/Relay.py
+++ Hardware/Relay.py
@@ -4,15 +4,23 @@ from Hardware.DeviceType import DeviceType
 
 
 class Relay(Device):
+    # тип реле "нормально открытое"
     NORMALLY_OPEN_TYPE = "NORMALLY_OPEN"
+    # тип реле "нормально закрытое"
     NORMALLY_CLOSED_TYPE = "NORMALLY_CLOSED"
+    # дефолтный тип реле
     DEFAULT_RELAY_TYPE = NORMALLY_OPEN_TYPE
 
+    # состояние реле "открыто"
     OPEN_STATE = "OPEN"
+    # состояние реле "закрыто"
     CLOSED_STATE = "CLOSED"
+    # дефолтное состояние реле
     DEFAULT_STATE = OPEN_STATE
 
-    def __init__(self, id:str, pin:int, mqtt_topic:str, name=None:str, description=None:str, relay_type=DEFAULT_RELAY_TYPE:str, state=DEFAULT_STATE:str):
+    def __init__(self, id: str, pin: int, mqtt_topic: str,
+                 name=None: str, description=None: str,
+                 relay_type=DEFAULT_RELAY_TYPE: str, state=DEFAULT_STATE: str):
         self.pin = onionGpio.OnionGpio(pin)
         self.mqtt_topic = mqtt_topic
         self.relay_type = relay_type if relay_type != None else DEFAULT_RELAY_TYPE
