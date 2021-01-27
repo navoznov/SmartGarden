ca3fbab52bb3f88fd70960cd9eec82ba69ce75a5
Navoznov Ivan <ivan.navoznov@gmail.com>
2021-01-27 20:30:12 +0300

    добавлен параметр name в конструктор Device

--- Hardware/Device.py
+++ Hardware/Device.py
@@ -1,5 +1,6 @@
 class Device:
-    def __init__(self, id:str, device_type:str, description=None:str):
+    def __init__(self, id: str, device_type: str, name=None: str, description=None: str):
         self.id = id
         self.type = device_type
-        self.description = description
+        self.name = name if name != None else "Датчик пин #" + str(pin)
+        self.description = description if description != None else "Датчик " + str(pin)
