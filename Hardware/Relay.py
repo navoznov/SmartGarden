794deb3d61cdb8391199fe79c7ed491143e2f26c
Navoznov Ivan <ivan.navoznov@gmail.com>
2021-01-27 16:20:59 +0300

    добавлен класс реле

--- Hardware/Relay.py
+++ Hardware/Relay.py
@@ -1,7 +1,14 @@
 import onionGpio
 
+
 class Relay:
-    def __init__(self, id, pin, state, mqtt_topic, name=None, description=None):
+    NORMALLY_OPEN_TYPE = "NORMALLY_OPEN"
+    NORMALLY_CLOSED_TYPE = "NORMALLY_CLOSED"
+
+    OPEN_STATE = "OPEN"
+    CLOSED_STATE = "CLOSED"
+
+    def __init__(self, id:str, pin:int, mqtt_topic:str, name=None:str, description=None:str, type=NORMALLY_OPEN_TYPE:str, state=OPEN_STATE:str):
         self.__id = id
         self.__pin = onionGpio.OnionGpio(pin)
         self.__state = state
