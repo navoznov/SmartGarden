14eb580ea66bbf42876bb9dc4c459dfbf07c8101
Navoznov Ivan <ivan.navoznov@gmail.com>
2021-01-27 16:02:07 +0300

    добавлено получение конфига и создание списка устройст

---
+++ main.py
@@ -0,0 +1,19 @@
+from GardenConfigParser import GardenConfigParser
+from DeviceBuilder import DeviceBuilder
+
+# получаем конфиг
+config_json_str = open("gardenConfig.json").read()
+config = GardenConfigParser.parse(config_json_str)
+
+# получаем настройки устройств
+device_configs = config.get("devices", [])
+
+# составляем список устройств
+device_builder = new DeviceBuilder()
+# REFACTORING: переписать цилк в одну строку
+# devices = [build_device(x) for x in device_configs]
+devices = []
+for device_config in device_configs:
+    device = device_builder.build_device(device_config)
+    devices.append(device)
+
