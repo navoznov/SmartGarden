b5ad10fcd5a5c60f5e7ccb9ab9e0db8b7abba63f
Navoznov Ivan <ivan.navoznov@gmail.com>
2021-01-28 12:52:26 +0300

    добавлена возможнсоть строить линейные актуаторы по конфигу

--- DeviceBuilder.py
+++ DeviceBuilder.py
@@ -2,10 +2,11 @@ import Hardware.TemperatureSensor import TemperatureSensor
 import Hardware.Valve import Valve
 import Hardware.Relay import Relay
 import Hardware.Sensor import Sensor
+import Hardware.LinearActuator import LinearActuator
 import Hardware.DeviceType import DeviceType
 
-class DeviceBuilder:
 
+class DeviceBuilder:
 
     def build_device(self, device_config):
         id = device_config.get("id", None)
@@ -22,6 +23,13 @@ class DeviceBuilder:
         if device_type == DeviceType.RELAY:
             relay_type = device_config.get("relay_type", None)
             state = device_config.get("state", None)
-            return new Relay(id, pin, mqtt_topic, name, description, relay_type, state)
+            return new Relay(id, pin, mqtt_topic, name, description,
+                             relay_type, state)
         if device_type == DeviceType.SENSOR:
             return new Sensor(id, pin, mqtt_topic, name, description)
+        if device_type == DeviceType.LINEAR_ACTUATOR:
+            state = device_config.get("state", None)
+            open_close_timeout_in_sec = device_config.get(
+                "open_close_timeout_in_sec", None)
+            return new LinearActuator(id, pin, mqtt_topic, name, description,
+                                      state, open_close_timeout_in_sec)
