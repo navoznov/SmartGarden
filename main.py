815d623416a0afafad1163498c07df630f5b8d2b
Navoznov Ivan <ivan.navoznov@gmail.com>
2021-02-07 20:58:11 +0300

    обернул код в main()

--- main.py
+++ main.py
@@ -1,19 +1,24 @@
 from GardenConfigParser import GardenConfigParser
 from DeviceBuilder import DeviceBuilder
 
-# получаем конфиг
-config_json_str = open("gardenConfig.json").read()
-config = GardenConfigParser.parse(config_json_str)
 
-# получаем настройки устройств
-device_configs = config.get("devices", [])
+def main():
+    # получаем конфиг
+    config_json_str = open("gardenConfig.json").read()
+    config = GardenConfigParser.parse(config_json_str)
 
-# составляем список устройств
-device_builder = new DeviceBuilder()
-# REFACTORING: переписать цилк в одну строку
-# devices = [build_device(x) for x in device_configs]
-devices = []
-for device_config in device_configs:
-    device = device_builder.build_device(device_config)
-    devices.append(device)
+    # получаем настройки устройств
+    device_configs = config.get("devices", [])
 
+    # составляем список устройств
+    device_builder = new DeviceBuilder()
+    # REFACTORING: переписать цилк в одну строку
+    # devices = [build_device(x) for x in device_configs]
+    devices = []
+    for device_config in device_configs:
+        device = device_builder.build_device(device_config)
+        devices.append(device)
+
+
+if __name__ == '__main__':
+    main()
