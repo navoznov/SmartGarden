90a59a691120e9b3bf04794b5fc3a06ffac139e2
Navoznov Ivan <ivan.navoznov@gmail.com>
2021-02-24 23:55:06 +0300

    рефакторинг valveStates

--- Hardware/Valve.py
+++ Hardware/Valve.py
@@ -4,28 +4,24 @@
 # import onionGpio
 from hardware.device import Device
 import hardware.deviceType as deviceType
+import hardware.valveStates as valveStates
 
 class Valve(Device):
     """Кран"""
 
-    # состояние крана "полностью открыт"
-    OPEN_STATE = "OPEN"
-    # состояние крана "полностью закрыт"
-    CLOSED_STATE = "CLOSED"
-
-    def __init__(self, id: str, pin: str, state: str, mqtt_topic: str, name: str, description: str=None):
+    def __init__(self, id: str, pin: str, state: str = None, mqtt_topic: str, name: str, description: str=None):
         self.pin = pin
-        self.state = state
+        self.state = valveStates.OPENED if state is None else state
         self.mqtt_topic = mqtt_topic
 
         super().__init__(id, deviceType.VALVE, name, description)
 
     def open(self):
-        self.state = Valve.OPEN_STATE
+        self.state = valveStates.OPENED
         # TODO: установить пин в 1
         # TODO: уведомление об открытии
 
     def close(self):
-        self.state = Valve.CLOSED_STATE
+        self.state = valveStates.CLOSED
         # TODO: установить пин в 0
         # TODO: уведомление о закрытии
---
+++ Hardware/valveStates.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# состояние крана "полностью открыт"
+OPENED = "OPENED"
+
+# состояние крана "полностью закрыт"
+CLOSED = "CLOSED"
