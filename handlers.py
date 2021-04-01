8d42014f216109185cc8bf61503440e5e69944e1
Navoznov Ivan <ivan.navoznov@gmail.com>
2021-04-01 18:44:44 +0300

    поправил ошибку

--- handlers.py
+++ handlers.py
@@ -129,7 +129,7 @@ def debug_set_pin_value(update: Update, context: CallbackContext) -> int:
 def __get_status_text() -> str:
     devices = garden.get_devices()
     statuses = [d.get_status() for d in devices]
-    return '\n'.join(statuses)
+    return '\n'.join(statuses) if len(statuses) else ''
 
 
 def __get_keyboard() -> List[List[str]]:
