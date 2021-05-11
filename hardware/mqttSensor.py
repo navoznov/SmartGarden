874a2eedc2182d14674c1f14927cafe6d72a8c04
Navoznov Ivan <ivan.navoznov@gmail.com>
2021-05-11 10:45:36 +0300

    корректная работа mqtt клиента в MqttSensor

--- hardware/mqttSensor.py
+++ hardware/mqttSensor.py
@@ -3,9 +3,10 @@ import paho.mqtt.client as mqtt
 
 class MqttSensor(Device):
     def __init__(self, id, device_type, mqtt_topic, mqtt_server, name, description):
+        self.__sensor_value = None
+
         self.mqtt_topic = mqtt_topic
         self.__mqtt_server = mqtt_server
-        self.__sensor_value = None
         # TODO: использовать один инстанс mqtt-клиента для всех девайсов (и вообще для всех нужд)
         self.__mqtt_client = mqtt.Client(f'mqtt_{id}')
 
@@ -15,13 +16,16 @@ class MqttSensor(Device):
             print(f'Ошибка подключения к серверу MQTT: {self.mqtt_server}')
 
         self.__mqtt_client.subscribe(self.mqtt_topic)
-        self.on_message = self.__handle_on_message
+
+        def handle_on_message(client, userdata, message):
+            value = message.payload.decode('utf-8')
+            self.__sensor_value = value
+
+        self.__mqtt_client.on_message = handle_on_message
+        self.__mqtt_client.loop_start()
 
         super().__init__(id, device_type, name, description)
 
-
-    def __handle_on_message(self, client, userdata, message):
-        self.__sensor_value = message
 
     def get_value(self):
         return self.__sensor_value
