fe10431145abff1d1bd9d9d08fb0c4ce2e8fd7ec
Navoznov Ivan <ivan.navoznov@gmail.com>
2021-05-11 10:43:28 +0300

    добавлен аргумент командной строки "список идентификаторов администраторов"

--- options.py
+++ options.py
@@ -1,9 +1,11 @@
 import sys, getopt
+from typing import List
 
 
 class Options:
-    def __init__(self, telegram_bot_token: str, mqtt_server: str, db_filename: str, is_debug_mode: bool):
+    def __init__(self, telegram_bot_token: str, admin_ids: List[int], mqtt_server: str, db_filename: str, is_debug_mode: bool):
         self.telegram_bot_token = telegram_bot_token
+        self.admin_ids = admin_ids          # идентификаторы юзеров в телеграме, которые имеют право управлять теплицей
         self.mqtt_server = mqtt_server
         self.db_filename = db_filename
         self.is_debug_mode = is_debug_mode
@@ -11,9 +13,9 @@ class Options:
 
 def __parse() -> Options:
     try:
-        longopts = ["bot-token=", "mqtt-server=", "db-filename=", "debug-mode="]
+        longopts = ["bot-token=", "admin-ids=", "mqtt-server=", "db-filename=", "debug-mode="]
         argv = sys.argv[1:]
-        opts, args = getopt.getopt(argv, "t:m:b:d", longopts)
+        opts, args = getopt.getopt(argv, "t:a:m:b:d", longopts)
         args = {}
         for a, v in opts:
             aa = a.replace('--', '')
@@ -21,7 +23,10 @@ def __parse() -> Options:
     except getopt.GetoptError as e:
         sys.exit(2)
 
-    options = Options(args["bot-token"], args["mqtt-server"], args["db-filename"], bool(args.get("debug-mode", False)))
+    admin_ids = [int(x) for x in args.get("admin-ids", "").split(',')]
+    is_debug_mode = bool(args.get("debug-mode", False))
+
+    options = Options(args["bot-token"], admin_ids, args["mqtt-server"], args["db-filename"], is_debug_mode)
     return options
 
 
