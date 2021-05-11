1e56378e71e82ab32fd9be95681cb999443e80d3
Navoznov Ivan <ivan.navoznov@gmail.com>
2021-05-11 09:09:38 +0300

    добавлен аргумент командной строки debug-mode

--- options.py
+++ options.py
@@ -2,17 +2,18 @@ import sys, getopt
 
 
 class Options:
-    def __init__(self, telegram_bot_token: str, mqtt_server: str, db_filename: str):
+    def __init__(self, telegram_bot_token: str, mqtt_server: str, db_filename: str, is_debug_mode: bool):
         self.telegram_bot_token = telegram_bot_token
         self.mqtt_server = mqtt_server
         self.db_filename = db_filename
+        self.is_debug_mode = is_debug_mode
 
 
 def __parse() -> Options:
     try:
-        longopts = ["bot-token=", "mqtt-server=", "db-filename="]
+        longopts = ["bot-token=", "mqtt-server=", "db-filename=", "debug-mode="]
         argv = sys.argv[1:]
-        opts, args = getopt.getopt(argv, "t:m:d", longopts)
+        opts, args = getopt.getopt(argv, "t:m:b:d", longopts)
         args = {}
         for a, v in opts:
             aa = a.replace('--', '')
@@ -20,7 +21,7 @@ def __parse() -> Options:
     except getopt.GetoptError as e:
         sys.exit(2)
 
-    options = Options(args["bot-token"], args["mqtt-server"], args["db-filename"])
+    options = Options(args["bot-token"], args["mqtt-server"], args["db-filename"], bool(args.get("debug-mode", False)))
     return options
 
 
